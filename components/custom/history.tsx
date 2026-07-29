f0a74215db440abc4fb140ac15b60fecbafe6551
Devansh Singh Parmar <devansh18dp@gmail.com>
2026-07-29 10:56:50 +0530

    schema

---
+++ components/custom/history.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+import cx from "classnames";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+import { User } from "next-auth";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import useSWR from "swr";
+
+import { Chat } from "@/db/schema";
+import { fetcher, getTitleFromChat } from "@/lib/utils";
